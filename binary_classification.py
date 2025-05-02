f66788bd2cbef348c93e733ab42aef0b77f62f66
Ksenia Biazruchanka <kseniabezruchenko@gmail.com>
2025-05-02 14:58:24 +0200

    Binary Classification finished

--- binary_classification.py
+++ binary_classification.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets, models
+from torch.utils.data import random_split
+import os
+from tempfile import TemporaryDirectory
 
 data_root = "datasets"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -15,50 +18,98 @@ transform = transforms.Compose([
                          std=[0.229, 0.224, 0.225]),
 ])
 
-def train_network(network, loader, criterion, optimizer, n_epochs):
-    network.train()
+def train_network(network, dataloaders, dataset_sizes, criterion, optimizer, num_epochs=25):
 
-    for epoch in range(n_epochs):
-        running_loss = 0.0
-        correct = 0
-        total = 0
+    train_losses = []
+    train_accuracies = []
+    val_losses = []
+    val_accuracies = []
 
-        for X_batch, Y_batch in loader:
-            X_batch, Y_batch = X_batch.to(device), Y_batch.to(device)
+    with TemporaryDirectory() as tempdir:
+        best_network_params_path = os.path.join(tempdir, 'best_network_params.pt')
 
-            optimizer.zero_grad()
-            S = network(X_batch)
-            loss = criterion(S, Y_batch)
-            loss.backward()
-            optimizer.step()
+        torch.save(network.state_dict(), best_network_params_path)
+        best_acc = 0.0
 
-            running_loss += loss.item()
-            _, P = torch.max(S, 1)
-            correct += (P == Y_batch).sum().item()
-            total += Y_batch.size(0)
+        for epoch in range(num_epochs):
+            print(f'Epoch {epoch + 1}/{num_epochs}')
+            print('-' * 10)
 
-        acc = 100 * correct / total
-        print(f"[Epoch {epoch+1}/{n_epochs}] Loss: {running_loss:.4f} | Train Accuracy: {acc:.2f}%")
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    network.train()
+                else:
+                    network.eval()
 
-def compute_accuracy(network, loader):
+                running_loss = 0.0
+                running_corrects = 0
+
+                for X, Y in dataloaders[phase]:
+                    X = X.to(device)
+                    Y = Y.to(device)
+
+                    optimizer.zero_grad()
+
+                    # forward
+                    with torch.set_grad_enabled(phase == 'train'):
+                        S = network(X)
+                        _, P = torch.max(S, 1)
+                        loss = criterion(S, Y)
+
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # statistics
+                    running_loss += loss.item() * X.size(0)
+                    running_corrects += torch.sum(P == Y.data)
+
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+                if phase == 'train':
+                    train_losses.append(epoch_loss)
+                    train_accuracies.append(epoch_acc.item())
+                else:
+                    val_losses.append(epoch_loss)
+                    val_accuracies.append(epoch_acc.item())
+
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+                # deep copy the model
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save(network.state_dict(), best_network_params_path)
+
+            print()
+
+        print(f'Best val Acc: {best_acc:4f}')
+
+        network.load_state_dict(torch.load(best_network_params_path, weights_only=True))
+    return network, train_losses, val_losses, train_accuracies, val_accuracies
+
+def compute_accuracy(network, loader, print_result=True):
     network.eval()
     correct = 0
     total = 0
 
     with torch.no_grad():
-        for X_batch, Y_batch in loader:
-            X_batch, Y_batch = X_batch.to(device), Y_batch.to(device)
-            S = network(X_batch)
+        for X, Y in loader:
+            X, Y = X.to(device), Y.to(device)
+            S = network(X)
             _, P = torch.max(S, 1)
-            correct += (P == Y_batch).sum().item()
-            total += Y_batch.size(0)
+            correct += (P == Y).sum().item()
+            total += Y.size(0)
 
     acc = 100 * correct / total
-    print(f"Test Accuracy: {acc:.2f}%")
+    if print_result:
+        print(f"Test Accuracy: {acc:.2f}%")
+    return acc
 
 
 if __name__ == '__main__':
-    train_data = datasets.OxfordIIITPet(
+    trainval_data = datasets.OxfordIIITPet(
         root=data_root,
         split='trainval',
         target_types='binary-category',
@@ -74,9 +125,25 @@ if __name__ == '__main__':
     )
 
     # Data loaders
+    val_ratio = 0.2  # 20% for validation
+    train_size = int((1 - val_ratio) * len(trainval_data))
+    val_size = len(trainval_data) - train_size
+
+    train_data, val_data = random_split(trainval_data, [train_size, val_size])
     batch_size = 32
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+    dataloaders = {}
+    dataset_sizes = {}
+
+    dataloaders['train'] = train_loader
+    dataloaders['val'] = val_loader
+    dataloaders['test'] = test_loader
+
+    dataset_sizes['train'] = len(train_data)
+    dataset_sizes['val'] = len(val_data)
 
     # ResNet18
     network = models.resnet18(weights='DEFAULT')
@@ -87,6 +154,31 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(network.parameters(), lr=1e-4)
 
-    train_network(network, train_loader, criterion, optimizer, 3)
+    # Running for 25 epochs
+    network, train_losses, val_losses, train_accuracies, val_accuracies = train_network(
+        network, dataloaders, dataset_sizes, criterion, optimizer, num_epochs=25
+    )
+
+    import matplotlib.pyplot as plt
+
+    plt.figure(facecolor='white')
+    plt.plot(train_losses, label='Train Loss')
+    plt.plot(val_losses, label='Validation Loss')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss over epochs')
+    plt.show()
+
+    plt.figure(facecolor='white')
+    plt.plot(train_accuracies, label='Train Accuracy')
+    plt.plot(val_accuracies, label='Validation Accuracy')
+    plt.legend()
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.9, 1.02])
+    plt.title('Accuracy over epochs')
+    plt.show()
+
     compute_accuracy(network, test_loader)
 
